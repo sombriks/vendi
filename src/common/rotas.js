4f91400c581a6c09dfd4cd7cbc5b6201a1fb572b
Leonardo Silveira <sombriks@gmail.com>
2020-02-21 23:07:29 -0300

    .

--- src/common/rotas.js
+++ src/common/rotas.js
@@ -1,43 +1,43 @@
 // no commons fica o código em comum ao main e ao renderer
 export default {
-  PAINEL_TESTE: {
-    label: "Painel de testes",
-    path: "/test-panel"
-  },
-  ENTRADA_OPERADOR: {
-    label: "Seleção de operador",
-    path: "/entrada-operador"
-  },
-  CONSULTA_PRODUTO: {
-    label: "Consulta de produtos",
-    path: "/consulta-produto"
-  },
-  SAIDA_OPERADOR: {
-    label: "Saída de operador",
-    path: "/saida-operador"
-  },
-  REFORCO_CAIXA: {
-    label: "Reforço de caixa",
-    path: "/reforco-caixa"
-  },
-  SANGRIA: {
-    label: "Sangria",
-    path: "/sangria"
-  },
-  ABERTURA_CAIXA: {
-    label: "Abertura de caixa",
-    path: "/abertura-caixa"
-  },
-  CONSULTA_CUPOM: {
-    label: "Consulta de cupom",
-    path: "/consulta-cupom"
-  },
-  CANCELA_ITEM: {
-    label: "Cancelar item",
-    path: "/cancela-item"
-  },
-  CANCELA_CUPOM: {
-    label: "Cancelar cupom",
-    path: "/cancela-cupom"
-  }
+	PAINEL_TESTE: {
+		label: "Painel de testes",
+		path: "/test-panel"
+	},
+	ENTRADA_OPERADOR: {
+		label: "Seleção de operador",
+		path: "/entrada-operador"
+	},
+	CONSULTA_PRODUTO: {
+		label: "Consulta de produtos",
+		path: "/consulta-produto"
+	},
+	SAIDA_OPERADOR: {
+		label: "Saída de operador",
+		path: "/saida-operador"
+	},
+	REFORCO_CAIXA: {
+		label: "Reforço de caixa",
+		path: "/reforco-caixa"
+	},
+	SANGRIA: {
+		label: "Sangria",
+		path: "/sangria"
+	},
+	ABERTURA_CAIXA: {
+		label: "Abertura de caixa",
+		path: "/abertura-caixa"
+	},
+	CONSULTA_CUPOM: {
+		label: "Consulta de cupom",
+		path: "/consulta-cupom"
+	},
+	CANCELA_ITEM: {
+		label: "Cancelar item",
+		path: "/cancela-item"
+	},
+	CANCELA_CUPOM: {
+		label: "Cancelar cupom",
+		path: "/cancela-cupom"
+	}
 };
